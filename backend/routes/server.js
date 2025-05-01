cf27e9cfdf22ffdd36fad1faa703018342f7b2c1
JiuZhuuu <154327378+JiuZhuuu@users.noreply.github.com>
2025-05-01 14:22:10 +0800

    setting up mongo

--- backend/routes/server.js
+++ backend/routes/server.js
@@ -13,7 +13,13 @@ app.use(cors());
 app.use(express.json());
 
 // Database connection
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
 
 // Routes
 app.use("/api/auth", authRoutes);
