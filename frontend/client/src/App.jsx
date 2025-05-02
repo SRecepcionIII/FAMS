2cafbdc3d5999fd8875d602351510d80b22312f6
JiuZhuuu <154327378+JiuZhuuu@users.noreply.github.com>
2025-05-02 16:30:12 +0800

    nodemon

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,9 +1,23 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/")
+      .then(response => setMessage(response.data))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
-  <h1>Finance and Accounting Management System</h1>
-  )
+    <div>
+      <h1>Financial &<br></br> Accounting</h1>
+      <p>{message}</p>
+    </div>
+  );
+
 }
 
 export default App;
