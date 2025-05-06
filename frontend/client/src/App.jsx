46273702500539150fd9a8defdf1349e44984491
JiuZhuuu <154327378+JiuZhuuu@users.noreply.github.com>
2025-05-06 08:20:58 +0800

    updated

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,23 +1,15 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import './App.css';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 
-function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    axios.get("http://localhost:5000/")
-      .then(response => setMessage(response.data))
-      .catch(error => console.error(error));
-  }, []);
+export default function App() {
 
   return (
-    <div>
-      <h1>Financial &<br></br> Accounting</h1>
-      <p>{message}</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
-
 }
-
-export default App;
