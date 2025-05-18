612ee8fbb5e681cb91dd3419340fdae5786933bf
JiuZhuuu <154327378+JiuZhuuu@users.noreply.github.com>
2025-05-18 19:49:06 +0800

    cors origin fix

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "https://fams-m6yv.onrender.com" }));
 app.use(express.json());
 
 // MongoDB connection
